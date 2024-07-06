6e1d56e69aec9f82f6343ba87b185d582bb8b27f
mohamed elshafey <elshafeym170@gmail.com>
2024-07-06 15:58:01 +0300

    Create rabbitmq shared project

---
+++ Shared/RabbitMq/IEventBus.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Shared.Primitives;
+
+namespace Shared.RabbitMq;
+
+public interface IEventBus
+{
+    Task SendCommand<T>(T command) where T : IRequest;
+    void Publish<T>(T @event) where T : IDomainEvent;
+    void Subscribe<T, TH>() where T : IDomainEvent
+        where TH : IEventHandler<T>;
+
+}
---
+++ Shared/RabbitMq/IEventHandler.cs
@@ -0,0 +1,13 @@
+﻿
+using Shared.Primitives;
+
+namespace Shared.RabbitMq;
+
+public interface IEventHandler<in TEvent> : IEventHandler where TEvent : IDomainEvent
+{
+    Task Handle(TEvent @event);
+}
+
+public interface IEventHandler
+{
+}
